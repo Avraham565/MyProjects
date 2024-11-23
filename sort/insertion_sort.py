db5f7755a41db4bd73ff17421a9097f4ba68d269
Avraham565 <israel15054@gmail.com>
2024-11-23 23:05:11 +0200

    insertion_sort.py

--- sort/insertion_sort.py
+++ sort/insertion_sort.py
@@ -1,29 +1,3 @@
-# def insertion_sort(arr):
-#     comparisons_count = 0
-#     initializations_count = 0
-
-#     for i in range(1, len(arr)):
-#         key = arr[i]
-#         j = i-1
-#         while j >= 0:
-#             comparisons_count += 1
-#             if arr[j] > key:
-#                 initializations_count += 1
-#                 arr[j+1] = arr[j]
-#                 comparisons_count -= 1
-
-#             elif arr[j] < key:
-#                 initializations_count += 1
-#                 arr[j+1] = key
-#                 break
-#             comparisons_count += 1
-#             initializations_count += 1
-#             j -= 1
-
-#         arr[j+1] = key
-
-#     return comparisons_count,initializations_count
-
 
 def insertion_sort(arr):
     comparisons_count = 0
